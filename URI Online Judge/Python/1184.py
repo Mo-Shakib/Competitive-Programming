1ed53cc72466acedd5235d0190dfa73f0f911185
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-06-17 02:43:33 +0600

    Create 1184.py

---
+++ URI Online Judge/Python/1184.py
@@ -0,0 +1,13 @@
+op = input()
+total = 0
+count = 0
+for row in range(12):
+    for colum in range(12):
+        value = float(input())
+        if (colum < row):
+            total += value
+            count += 1
+if op == 'S':
+    print('{:.1f}'.format(total))
+else:
+    print('{:.1f}'.format(total/count))
