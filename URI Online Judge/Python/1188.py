e066613d024bd9577483e734132bec9487f6bf9d
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-06-17 17:01:48 +0600

    Create 1188.py

---
+++ URI Online Judge/Python/1188.py
@@ -0,0 +1,14 @@
+# Problem link: https://www.urionlinejudge.com.br/judge/en/problems/view/1188
+op = input()
+total = 0
+count = 0
+for row in range(12):
+    for colum in range(12):
+        value = float(input())
+        if (colum < row) and ((row + colum) > 11):
+            total += value
+            count += 1
+if op == 'S':
+    print('{:.1f}'.format(total))
+else:
+    print('{:.1f}'.format(total/count))
