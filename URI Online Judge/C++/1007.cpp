c448bc77185400ce0e754907ef0917254dae9b41
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-06-19 00:09:30 +0600

    Create 1007.cpp

---
+++ URI Online Judge/C++/1007.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, result;
+    cin >> a >> b >> c >> d;
+    result = a * b - c * d;
+    cout << "DIFERENCA = " << result << endl;
+    return 0;
+
+}
