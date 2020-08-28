838995294bcb3c55869908e7b600c9de1bfbd391
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-08-28 11:57:27 +0600

    :D

--- Toph/Full pyramid.py
+++ Toph/Full pyramid.py
@@ -3,5 +3,8 @@ for i in range(a):
     for j in range(a-i-1):
         print(end=" ")
     for j in range(i+1):
-        print("*",end=' ')
+        if j == a:
+            print("*")
+        else:
+            print("*",end=' ')
     print()
