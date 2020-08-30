6e6df63951c2d7cb43b603f0729488a8aa6cb9a5
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-08-30 21:44:30 +0600

    Update A.py

--- CodeForces/Codeforces Round #666 (Div. 2)/A.py
+++ CodeForces/Codeforces Round #666 (Div. 2)/A.py
@@ -3,10 +3,26 @@ t = int(input())
 for i in range(t):
     t_2 = int(input())
     total_string = ''
-    for j in range(t):
+    dicts = {}
+    for j in range(t_2):
         string = input()
         total_string += string
-    if len(total_string) % t_2 == 0:
+    for k in total_string:
+        if k in dicts:
+            dicts[k] += 1
+        else:
+            dicts[k] = 1
+    total = 0
+    for l in dicts.values():
+        total += l
+    temp = 0
+    for m in dicts.values():
+        if total % m == 0:
+            pass
+        else:
+            temp += 1
+    if temp == 0:
         print('YES')
     else:
         print('NO')
+    
