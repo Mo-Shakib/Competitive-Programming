b7ec8da788e06524d97adaaf9ffaff15251e7d11
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-08-30 14:06:39 +0600

    Update A - Stones on the Table.py

--- CodeForces/Python/A - Stones on the Table.py
+++ CodeForces/Python/A - Stones on the Table.py
@@ -3,12 +3,15 @@ stons = input()
 r = 0
 g = 0
 b = 0
+temp = stons[0]
+count = 0
 for i in range(t):
-    if stons[i-1] == stons[i]:
+    if stons[count] == stons[count+1]:
         if stons[i] == 'R':
             r += 1
         elif stons[i] == 'B':
             b += 1
         elif stons[i] == 'G':
             g += 1
+    count += 1
 print(r+g+b)
