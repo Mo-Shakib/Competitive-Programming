0ab261481dda1335a3d9c87f38c6697a73edb76c
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-09-04 16:28:36 +0600

    Update A.py

--- BRACU Traction Mock/A.py
+++ BRACU Traction Mock/A.py
@@ -1,13 +1,13 @@
+def func(x):
+    table = []
+    for j in range(1, 11):
+        for k in range(1, 11):
+            table.append(j * k)
+    if x in table:  out = 'Yes'
+    else:   out = 'No'
+    print(out)
+
 t = int(input())
-table = []
-for j in range(1, 11):
-    for k in range(1, 11):
-        table.append(j * k)
 for i in range(t):
     num = int(input())
-    if num in table:
-        print('Yes')
-    else:
-        print('No')
-    
-    
+    func(num)
