c4f2a80d5a5cf236d0907dc19c45a5c2bf6ba04d
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-09-07 04:21:26 +0600

    Create a+b.py

---
+++ HackerEarth/Practice/a+b.py
@@ -0,0 +1,8 @@
+# https://www.hackerearth.com/practice/basic-programming/complexity-analysis/time-and-space-complexity/practice-problems/algorithm/a-b-4/description/?layout=old
+
+try:
+    while True:
+        a,b = map(int,input().split())
+        print(a+b)
+except:
+    pass
