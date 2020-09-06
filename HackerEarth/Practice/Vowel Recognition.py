6c099d26a5e4cb6fa47f8a211ca1a2da98252dc1
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-09-07 05:05:43 +0600

    :aerial_tramway:

---
+++ HackerEarth/Practice/Vowel Recognition.py
@@ -0,0 +1,9 @@
+# https://www.hackerearth.com/practice/basic-programming/complexity-analysis/time-and-space-complexity/practice-problems/algorithm/vowel-game-f1a1047c/?layout=old
+
+string = 'baceb'
+lenth = len(string)
+sub = []
+count = lenth -1
+for i in string:
+    print(string[count:])
+    count -=1
