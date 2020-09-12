9cee887937a45e5a0ac4e828f5fae4159411a99d
Mohammad Shakib <50780268+iMohammadShakib@users.noreply.github.com>
2020-09-12 21:31:34 +0600

    Create A.py

---
+++ CodeForces/Codeforces Round #670 (Div. 2)/A.py
@@ -0,0 +1,11 @@
+t = int(input())
+a = []
+b = []
+for i in range(t):
+    lenth = int(input())
+    lista = list(map(int, input().split()))
+    if len(lista) % 2 == 0:
+        for i in lista:
+            
+
+    print(max(lista))
