631c8282a56b9442bfceee6af34766082f801b02
Shakib <code.shakb@gmail.com>
2020-10-08 10:20:47 +0600

    Create List comprehension.py

---
+++ HackerRank/Python/List comprehension.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/list-comprehensions/problem
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    # List Comprehensions in Python - Hacker Rank Solution START
+    output = []
+    abc = []
+    for X in range(x+1):
+        for Y in range(y+1):
+            for Z in range(z+1):
+                if X+Y+Z != n:
+                    abc = [X,Y,Z]
+                    output.append(abc)
+print(output)
