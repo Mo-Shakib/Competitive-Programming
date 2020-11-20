3aebb80c4be116058a4aa4cac0304c15fb18e799
Code-Shakib <imoshakib.bd@gmail.com>
2020-11-20 12:32:45 +0600

    happy coding

--- HackerRank/Python/Find a string.py
+++ HackerRank/Python/Find a string.py
@@ -1,16 +1,30 @@
 # https://www.hackerrank.com/challenges/find-a-string/problem
 def count_substring(string, sub_string):
-    x = string.find(sub_string)
+    N = len(string) 
+    M = len(sub_string) 
+    res = 0
+      
+    # A loop to slide pat[] one by one  
+    for i in range(N - M + 1): 
+          
+        # For current index i, check  
+        # for pattern match  
+        j = 0
+        while j < M: 
+            if (string[i + j] != sub_string[j]): 
+                break
+            j += 1
+  
+        if (j == M): 
+            res += 1
+            j = 0
+    return res
 
-
-    return x
 
 if __name__ == '__main__':
 
-    # string = input().strip()
-    string = 'ABCDCDC'
-    # sub_string = input().strip()
-    sub_string = 'CDC'
+    string = input().strip()
+    sub_string = input().strip()
     
     count = count_substring(string, sub_string)
     print(count)
---
+++ HackerRank/Python/Matrix Script.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/matrix-script/problem
+import re
+n, m = map(int, input().split())
+a = []
+
+for _ in range(n):
+    a.append(input()[:m])
+
+s = ''.join(''.join(e) for e in zip(*a))
+result = re.sub(r'([a-zA-Z0-9])([^a-zA-Z0-9]+)(?=[a-zA-Z0-9])', r'\1 ', s)
+print(result)
