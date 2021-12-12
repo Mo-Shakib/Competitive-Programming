5d494b473a7af409b4da9cdfcf1df651b4a109cd
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2021-12-12 07:22:15 +0600

    optimized solution: accepted

--- BRACU/BRACU Beginners Long Contest 06 (Binary manipulation) [Batch-3]/C.py
+++ BRACU/BRACU Beginners Long Contest 06 (Binary manipulation) [Batch-3]/C.py
@@ -1,16 +1,28 @@
 # https://vjudge.net/contest/472762#problem/C
 
-def solution(arr):
-    arr = sorted(arr)
-    result = 0
-    for i in range(len(arr)-1):
-        for j in range(i+1, len(arr)):
-            if arr[i] & arr[j] >= arr[i] ^ arr[j]:
-                result += 1                
-    print(result)
+# getting the position of the least significant digit
+def pos_lsd(num):
+    x = bin(num)[2:]
+    return len(str(x))
 
+def solution(arr):
+    my_dict = {}
+    result = 0
+    
+    for i in arr:
+        idx = pos_lsd(i)
+        if idx not in my_dict:
+            my_dict[idx] = [i]
+        else: my_dict[idx] += [i]
+
+    for x in my_dict.values():
+        if len(x) > 1:
+            result += (len(x)*(len(x)-1)//2)
+    
+    return result
+        
 n = int(input())
 for i in range(n):
     a = int(input())
     x = list(map(int, input().split()))
-    solution(x)
+    print(solution(x))
